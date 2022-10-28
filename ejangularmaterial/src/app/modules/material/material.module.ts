ed5fa218e67b28e61a5345e21fd20f62f865fc8a
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-10-28 10:28:56 -0500

    Probando esquemas de Angular Material

--- ejangularmaterial/src/app/modules/material/material.module.ts
+++ ejangularmaterial/src/app/modules/material/material.module.ts
@@ -5,16 +5,73 @@ import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormularioDireccionComponent } from './components/formulario-direccion/formulario-direccion.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { TreeComponent } from './components/tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { DragdropComponent } from './components/dragdrop/dragdrop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormularioDireccionComponent,
+    NavComponent,
+    TablaComponent,
+    DashboardComponent,
+    TreeComponent,
+    DragdropComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatTreeModule,
+    DragDropModule
   ],
   exports: [
     MatDatepickerModule,
+    MatButtonModule,
     MatFormFieldModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    // exportamos los componente que hemos creado
+    FormularioDireccionComponent,
+    TablaComponent,
+    DashboardComponent,
+    NavComponent,
+    TreeComponent,
+    DragdropComponent
   ]
 })
 export class MaterialModule { }
