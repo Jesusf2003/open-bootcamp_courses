a62f87f1acf9a8729f7f5343286c1a80222aaaae
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2023-03-01 13:10:51 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 // import Greeting from './components/pure/greeting';
 // import GreetingF from './components/pure/greetingF';
-// import TaskListComponent from './components/container/task-list';
-import ContactComponent from './components/pure/contact';
+import ExampleComponent from './hooks/Example1'
+import TaskListComponent from './components/container/task-list';
+// import ContactComponent from './components/pure/contact';
 
 function App() {
 
-  const contactTest = {
-    name: 'Fernando',
-    last_name: 'Canales',
-    email: 'fc@correo.com',
-    connected: false
-  }
+  // const contactTest = {
+  //   name: 'Fernando',
+  //   last_name: 'Canales',
+  //   email: 'fc@correo.com',
+  //   connected: false
+  // }
 
   return (
     <div className="App">
@@ -23,8 +24,10 @@ function App() {
         {/* Componente de ejemplo funcional */}
         {/* <GreetingF name="Martín"></GreetingF> */}
         {/* Componente de listado de tareas */}
-        {/* <TaskListComponent></TaskListComponent> */}
-        <ContactComponent contact={contactTest}></ContactComponent>
+        <TaskListComponent></TaskListComponent>
+        {/* <ContactComponent contact={contactTest}></ContactComponent> */}
+        {/* Ejemplos de uso de HOOKS */}
+        <ExampleComponent></ExampleComponent>
       </header>
     </div>
   );
---
+++ src/hooks/Example1.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const ExampleComponent = () => {
+
+  const initialValue = 0;
+  const initialPerson = {
+    name: 'Fernando',
+    email: 'fcr@email.com'
+  };
+
+  /*
+    El Valor inicial y persona inicial son parte del estado
+    del componente para así poder gestionar su cambio y
+    que éste se vea reflejado en la vista del componente.
+
+    const [variable, function] = useState(value)
+  */
+  const [count, setCount] = useState(initialValue);
+  const [person, setPerson] = useState(initialPerson);
+
+  /*
+    Función para actualizar el estado privado que contiene
+    el contador.
+  */
+  function incrementCounter() {
+    // function to Change(newValue)
+    setCount(count + 1);
+  }
+
+  /*
+    Función para actualizar el estado de persona en el
+    componente
+  */
+  function updatePerson(name) {
+    setPerson(
+      {
+        name: name,
+        email: name + '@gmail.com'
+      }
+    );
+  }
+
+  return (
+    <div>
+      <h1>*** Ejemplo de useState() ***</h1>
+      <h2>Contador: {count}</h2>
+      <h2>Datos de la persona: </h2>
+      <h3>Nombre: {person.name}</h3>
+      <h4>Email: {person.email}</h4>
+      {/* Bloque de botones para actualizar el estado del componente */}
+      <button onClick={incrementCounter()}>Incrementar contador</button>
+      <button onClick={updatePerson(person)}>Actualizar persona</button>
+    </div>
+  );
+}
+
+export default ExampleComponent;
